9978b24b037838266114b201eed8f50a820a68a7
lulanoise <lucrezia.rumore98@gmail.com>
2023-02-06 22:00:52 +0100

    int modified

--- src/it/develhope/stringhandling/Start.java
+++ src/it/develhope/stringhandling/Start.java
@@ -5,7 +5,7 @@ public class Start {
     public static void main(String[] args) {
         String string1 = "United Kingdom";
         String string2 = "Germany";
-        int index = string1.indexOf(' ');
+        int index = string1.indexOf(" ");
         String sub1 = string1.substring(0,index).toUpperCase();
         String sub2 = string1.substring(index,14).toUpperCase();
 
@@ -14,7 +14,4 @@ public class Start {
         System.out.println(charArray);
     }
 }
-
-
-        // concatenate the strings, then convert them to uppercase and finally transform into a char[]
      
